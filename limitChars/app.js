9b1472b40638f75a1180b4302b6fe35178dd9a2f
Peter Bacon Darwin <pete@bacondarwin.com>
2014-08-31 20:30:30 +0100

    fix(limitChars): doesn't actually use updateMessage

--- limitChars/app.js
+++ limitChars/app.js
@@ -14,14 +14,9 @@ angular.module('test-app', [])
 
   // Attach our limitChars function to the scope
   $scope.limitChars = limitChars;
-  
-  // Handle the Update Message button click
-  $scope.updateMessage = function updateMessage() {
-    $scope.limitedMessage = $scope.limitChars($scope.message, $scope.limit);
-  };
-  
+
   // Initialize the scope with some data
   $scope.message = "This is a very long sentence, that goes on and on and on";
   $scope.limit = 15;
-  
+
 });
--- limitChars/app.spec.js
+++ limitChars/app.spec.js
@@ -1,38 +1,46 @@
-describe('MainCtrl controller', function() {
+describe('test-app', function() {
 
-  var mainCtrl, $scope, limitCharsSpy;
-  
   // Load the test-app module
   beforeEach(module('test-app'));
 
-  beforeEach(inject(function($controller, $rootScope) {
-    
-    // Create a new child scope (we could have simply used the $rootScope)
-    $scope = $rootScope.$new();
-    
-    // Mock out the limitChars function
-    limitCharsSpy = jasmine.createSpy('limitChars').andReturn('XXX');
-    
-    // Instantiate our controller, passing in the scope
-    mainCtrl = $controller('MainCtrl', { $scope: $scope, limitChars: limitCharsSpy });
-    
-  }));
-  
-  it('should attach the limitChars service to the scope', function() {
-    
-    // Note that the limitChars method is attached to the scope, not the controller
-    expect($scope.limitChars).toBe(limitCharsSpy);
-  
-  });
-  
-  
-  describe('updateMessage method', function() {
-  
-    it('should update the limitedMessage with XXX', function() {
-      $scope.updateMessage();
-      expect(limitCharsSpy).toHaveBeenCalledWith($scope.message, $scope.limit);
-      expect($scope.limitedMessage).toEqual('XXX');
+  describe('MainCtrl controller', function() {
+
+    var mainCtrl, $scope, limitCharsMock;
+
+    beforeEach(inject(function($controller, $rootScope) {
+
+      // Store the scope for later
+      $scope = $rootScope;
+
+      // Mock out the limitChars function
+      limitCharsMock = angular.noop;
+
+      // Instantiate our controller, passing in the scope and services
+      mainCtrl = $controller('MainCtrl', {
+        $scope: $scope,
+        limitChars: limitCharsMock
+      });
+
+    }));
+
+    it('should attach the limitChars service to the scope', function() {
+
+      // Note that the limitChars method is attached to the scope, not the controller
+      expect($scope.limitChars).toBe(limitCharsMock);
+
     });
+
+  });
+
+  describe("limitChars", function() {
+
+    it("should truncate the string at the specified limit",
+        inject(function(limitChars) {
+
+      expect(limitChars('A very long string', 10))
+          .toEqual('A very ...');
+
+    }));
 
   });
 });

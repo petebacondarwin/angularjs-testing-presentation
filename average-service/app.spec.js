1aae94c9a931d05eee8f1373802933f727ec1714
Peter Bacon Darwin <pete@bacondarwin.com>
2014-08-31 20:29:43 +0100

    style(average): add a bit of whitespace

--- average-service/app.spec.js
+++ average-service/app.spec.js
@@ -1,7 +1,7 @@
 
 describe('average function', function() {
   var average;
-  
+
   beforeEach(module('average-app'));
   beforeEach(inject(function(_average_) {
     average = _average_;
@@ -10,11 +10,11 @@ describe('average function', function() {
   it('should return the average of the values in an array', function() {
     expect(average([1,2,3,4])).toBe(2.5);
   });
-  
+
   it('should return 0 for an empty array', function() {
     expect(average([])).toBe(0);
   });
-  
+
   it('should throw invalid parameter `values`, if it is not an array', function() {
     expect(function() { average(); }).toThrow('invalid parameter `values`');
     expect(function() { average("abc"); }).toThrow('invalid parameter `values`');
